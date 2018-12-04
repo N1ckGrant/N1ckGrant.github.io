45ed93ca8c41a31968dcc70abdb7f77a628aab5c
Nick <nickyshibaev@gmail.com>
2018-12-04 13:41:19 +0200

    scroll

--- js/main.js
+++ js/main.js
@@ -111,7 +111,6 @@ function table_vacancy() {
     var tabs = $('.table__tab');
     console.log($('[data-vacancy]'));
     $(tabs).on('click', function () {
-
         $('.table__body').removeClass('active');
         $('#' + $(this).attr('data-vacancy')).addClass('active');
     });
@@ -139,6 +138,95 @@ modal();
 
 $('.single-item').slick({
     dots: true
+});
+
+$(".scroll").click(function (event) {
+    event.preventDefault();
+    var full_url = this.href;
+    var parts = full_url.split("#");
+    var trgt = parts[1];
+    var target_offset = $("#" + trgt).offset();
+    var target_top = target_offset.top;
+
+    $('html, body').animate({ scrollTop: target_top }, { done: function done() {
+            $(this).animate().stop(true);
+            $("body").css("overflow-y", "auto");
+        }, duration: 1500 });
+});
+
+$('#container').hover(function () {
+    $(document).bind('mousewheel DOMMouseScroll', function () {
+        stopWheel();
+    });
+}, function () {
+    $(document).unbind('mousewheel DOMMouseScroll');
+});
+
+function stopWheel(e) {
+    if (!e) {
+        /* IE7, IE8, Chrome, Safari */
+        e = window.event;
+    }
+    if (e.preventDefault) {
+        /* Chrome, Safari, Firefox */
+        e.preventDefault();
+    }
+    e.returnValue = false; /* IE7, IE8 */
+}
+
+var tempScrollTop,
+    currentScrollTop = 0;
+var notScroll = false;
+$(window).scroll(function () {
+    var $height = $(window).scrollTop();
+    var elem = $('.hero');
+    var target_top = $("#process_block").offset().top;
+    tempScrollTop = currentScrollTop;
+    currentScrollTop = $(window).scrollTop();
+    //scrolling down
+
+    if (tempScrollTop < currentScrollTop && !notScroll && $height > 0 && $height < 20) {
+        // $("body").css("overflow","hidden");
+        console.log('прокрутка вниз');
+
+        $('html, body').animate({ scrollTop: target_top }, { done: function done() {
+                console.log('done');
+                $(this).animate().stop();
+                $("body").bind('mousewheel DOMMouseScroll', function () {
+                    stopWheel();
+                });
+            }, duration: 1500 });
+        $("body").bind('mousewheel DOMMouseScroll', function () {
+            console.log('helloooooo');
+            e.returnValue = true;
+        });
+        console.log('первый скролл прошел');
+        console.log('scroll down');
+        notScroll = true;
+    }
+
+    if (tempScrollTop > currentScrollTop && $(document).scrollTop() + $(window).height() > $(elem).offset().top && $(document).scrollTop() - $(elem).offset().top < $(elem).height() && $height > 500) {
+        //scrolling up
+        //          $("body").css("overflow","hidden");
+        $('html, body').animate({ scrollTop: 0 }, { done: function done() {
+                $("body").bind('mousewheel DOMMouseScroll', function () {
+                    stopWheel();
+                });
+                console.log('yyyyyy');
+                $('html, body').animate().stop(true);
+            }, duration: 1500 });
+        $("body").bind('mousewheel DOMMouseScroll', function () {
+            console.log('helloooooo');
+            e.returnValue = true;
+        });
+
+        console.log("вверх прокрутка");
+        notScroll = false;
+        console.log('второй скролл');
+    } else {
+
+        console.log(notScroll);
+    }
 });
 
 /***/ })
