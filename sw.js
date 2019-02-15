a148b0e957a6367425c2cced9245d2ae65657bf6
Nick <nickyshibaev@gmail.com>
2019-02-15 17:49:19 +0200

    adaptive

--- sw.js
+++ sw.js
@@ -1,23 +1,2 @@
-// sw.js
-self.addEventListener('install', e => {
-    e.waitUntil(
-        // после установки service worker
-        // открыть новый кэш
-        caches.open('my-pwa-cache').then(cache => {
-            // добавляем все URL ресурсов, которые хотим закэшировать
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/IGT_crypto.html',
-                '/project.html',
-                '/vacancy.html',
-                '/about.html',
-                '/img/introBG.gif',
-                '/css/main.css',
-                '/css/vendor.min.css',
-                '/js/main.js',
-                '/js/index.js',
-            ]);
-        })
-    );
-});
+‘use strict’;
+importScripts(‘sw-toolbox.js’); toolbox.precache([“index.html”,”css/main.css”]); toolbox.router.get(‘/img/*’, toolbox.cacheFirst); toolbox.router.get(‘/*’, toolbox.networkFirst, { networkTimeoutSeconds: 5});
