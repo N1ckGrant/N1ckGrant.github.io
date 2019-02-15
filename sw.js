47f30a7ff9dba09f535c78040d12e2db77ff8005
Nick <nickyshibaev@gmail.com>
2019-02-15 17:53:58 +0200

    adaptive

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-‘use strict’;
-importScripts(‘sw-toolbox.js’); toolbox.precache([“index.html”,”css/main.css”]); toolbox.router.get(‘/img/*’, toolbox.cacheFirst); toolbox.router.get(‘/*’, toolbox.networkFirst, { networkTimeoutSeconds: 5});
+'use strict';
+importScripts('sw-toolbox.js'); toolbox.precache(["index.html","css/main.css"]); toolbox.router.get('/img/*', toolbox.cacheFirst); toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
